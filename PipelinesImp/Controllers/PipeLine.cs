8d7e523e9f2d908a335d1e5e2d0041c0e0da425a
YASSINE WALID <y.walid@Alexsys.solutions>
2024-10-07 16:16:21 +0100

    Add project files.

---
+++ PipelinesImp/Controllers/PipeLine.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace PipelinesImp.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PipeLine : ControllerBase
+    {
+        [HttpGet(Name = "TestPipeLines")]
+        public ActionResult Get()
+        {
+            return Ok("hello");
+        }
+    }
+}
